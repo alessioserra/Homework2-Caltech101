32d009644530236dc6ae1f24d7c3fe22dc2e09d9
zierp <zierp@LAPTOP-4TV42EQM>
2020-05-06 11:04:11 +0200

    caltech.py imported

--- caltech_dataset.py
+++ caltech_dataset.py
@@ -2,9 +2,11 @@ from torchvision.datasets import VisionDataset
 
 from PIL import Image
 
+import numpy as np
 import os
 import os.path
 import sys
+from sklearn.model_selection._split import StratifiedShuffleSplit
 
 
 def pil_loader(path):
@@ -15,35 +17,33 @@ def pil_loader(path):
 
 
 class Caltech(VisionDataset):
+    
     def __init__(self, root, split='train', transform=None, target_transform=None):
         super(Caltech, self).__init__(root, transform=transform, target_transform=target_transform)
 
-        self.split = split # This defines the split you are going to use
-                           # (split files are called 'train.txt' and 'test.txt')
-
-        '''
-        - Here you should implement the logic for reading the splits files and accessing elements
-        - If the RAM size allows it, it is faster to store all data in memory
-        - PyTorch Dataset classes use indexes to read elements
-        - You should provide a way for the __getitem__ method to access the image-label pair
-          through the index
-        - Labels should start from 0, so for Caltech you will have lables 0...100 (excluding the background class) 
-        '''
+        self.split = split  # This defines the split you are going to use
+                            # (split files are called 'train.txt' and 'test.txt')
+        
+        self.dataset = {}
+        self.counter = 0
+        classes_dict = {}
+        class_counter = 0
+        path = 'Caltech101/'+split+'.txt'
+        indexes = set(np.loadtxt(path,dtype=str))
+        classes = os.listdir(root)
+        classes.remove('BACKGROUND_Google')
+        
+        for class_ in classes:
+            classes_dict[class_] = class_counter
+            class_counter += 1
+            images = os.listdir(root+'/'+class_)
+            for image in images:
+                if class_+'/'+image in indexes:
+                    self.dataset[self.counter] = (pil_loader(root+'/'+class_+'/'+image),classes_dict[class_])
+                    self.counter += 1
 
     def __getitem__(self, index):
-        '''
-        __getitem__ should access an element through its index
-        Args:
-            index (int): Index
-
-        Returns:
-            tuple: (sample, target) where target is class_index of the target class.
-        '''
-
-        image, label = ... # Provide a way to access image and label via index
-                           # Image should be a PIL Image
-                           # label can be int
-
+        image, label = self.dataset[index]
         # Applies preprocessing when accessing the image
         if self.transform is not None:
             image = self.transform(image)
@@ -51,9 +51,18 @@ class Caltech(VisionDataset):
         return image, label
 
     def __len__(self):
-        '''
-        The __len__ method returns the length of the dataset
-        It is mandatory, as this is used by several other components
-        '''
-        length = ... # Provide a way to get the length (number of elements) of the dataset
-        return length
+        return self.counter
+    
+    def _getsplit_(self, train_size):
+        images, labels = [], []
+        sss = StratifiedShuffleSplit(1,train_size=train_size)
+
+        for item in self.dataset.values():
+            images.append(item[0])
+            labels.append(item[1])
+
+        for x, y in sss.split(images,labels):
+            train_indexes = x
+            val_indexes = y 
+
+        return train_indexes, val_indexes
